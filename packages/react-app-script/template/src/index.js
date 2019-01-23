7413daa12622c39c2ec26eac3bd38128f8e901cf
YingQQQ <yingqi2008win@gmail.com>
2019-01-23 16:51:07 +0800

    add html template folder

---
+++ packages/react-app-script/template/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
